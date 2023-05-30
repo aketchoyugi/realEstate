08dbc9a45cc82e0e4d5572537d3c2f86316874d9
aketchoyugi <46833353+aketchoyugi@users.noreply.github.com>
2023-05-30 14:10:38 +0000

    addin the house admin.py document

--- houses/admin.py
+++ houses/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import House
 
 # Register your models here.
+
+class HouseAdmin(admin.ModelAdmin):
+  list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'SalesPerson')
+  list_display_links = ('id', 'title')
+  list_filter = ('SalesPerson',)
+  list_editable = ('is_published',)
+  search_fields = ('title', 'description', 'address', 'city', 'county', 'zipcode', 'price')
+  list_per_page = 25
+
+admin.site.register(House, HouseAdmin)
