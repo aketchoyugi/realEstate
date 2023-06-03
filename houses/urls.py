ba802efe93918980f4423b8f5154e22ea28ef44a
aketchoyugi <46833353+aketchoyugi@users.noreply.github.com>
2023-06-03 11:49:37 +0000

    urls edit

--- houses/urls.py
+++ houses/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'houses'
+
 urlpatterns = [
     path('', views.index, name='houses'),
     path('<int:house_id>', views.house, name='house'),
